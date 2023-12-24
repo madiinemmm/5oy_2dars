a830bd0c837357980eb4c1cb1d02739ea116f3fd
madiinemm <uraimovamadina179@gmail.com>
2023-12-24 19:06:11 +0500

    masalalar ishlandi

---
+++ index.js
@@ -0,0 +1,61 @@
+
+// 1
+// let arr = [4, 6, 8, 9, 7];
+// let sum = 0;
+// for (const iterator of arr) {
+//     if(iterator % 2 == 0) {
+//     ret = iterator ** 2;
+//     } console.log(ret);
+// } 
+
+// 2
+// function  createCharArr (arr) {
+//     let strArr = [];
+//     arr.forEach(element => {
+//         strArr.push(element.charAt(0));
+//     });
+
+//     return strArr;
+    
+// }
+// console.log(createCharArr(["hello", "bye"]));
+
+// 3
+// let arr = [2, 5, 8, 9, 4, 6];
+// let sum = 0;
+// for (const iterator of arr) {
+//     sum += iterator;
+// }  console.log(sum);
+
+// 4
+// let n = 2;
+// let counter = 0
+// for(let i =1; i<=n; i++) {
+//     if(n % i == 0) {
+//         counter++;
+//     }
+// } 
+// if(counter == 2) {
+//     console.log("tub son");
+// } else {
+//     console.log("tub son emas");
+// }
+
+// 5
+// let a = 237;
+// let sum = 1;
+// let b = a % 10;
+// let o = ((a % 100) - b) / 10;
+// let y = (a / 100) - (a % 100) / 100;
+// if(b % 2 == 1) {
+//   sum *= b;
+// } 
+// if(o % 2 == 1) {
+//     sum *= o;
+//   } 
+//   if(y % 2 == 1) {
+//     sum *= y;
+//   } 
+//   console.log(sum);
+
+
